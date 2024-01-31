8dce5876de46160eb12b78020bdc4b7eec7852ef
ivane <gdzelishviliv@gmail.com>
2024-01-31 20:50:21 +0400

    finally fixed background problem

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const themeToggler=document.querySelector(".daynight-btn");
-themeToggler.addEventListener("click",()=>{
-    document.body.classList.toggle("dark-theme-variables");
-    const is_dark_mode=document.body.classList.contains(
-        "dark-theme-variables"
-    )
-    window.localStorage.setItem("dark-mode",is_dark_mode)
+const themeToggler = document.querySelector(".daynight-btn");
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+  const is_dark_mode = document.body.classList.contains("dark-theme-variables");
+  window.localStorage.setItem("dark-mode", is_dark_mode);
 });
